dc9d0d1ce37c4186069fb29d4b779fd4298b19ce
Pepe17031 <99272584+Pepe17031@users.noreply.github.com>
2023-10-08 06:16:49 +0300

    update

---
+++ watcher/coin_watcher.py
@@ -0,0 +1,43 @@
+from binance import ThreadedDepthCacheManager
+import psycopg2 as pg
+import pandas as pd
+
+
+def main():
+
+    coin_symbol = "BTCUSDT"
+    try:
+        conn = pg.connect(
+            host='db',
+            database='coin_watcher',
+            port=5432,
+            user='user',
+            password='password'
+        )
+
+        cursor = conn.cursor()
+        print("Подключение к postgres установленно.")
+
+        dcm = ThreadedDepthCacheManager()
+        dcm.start()
+
+        def on_input_message(depth_cache):
+
+            symbol = depth_cache.symbol
+            time = depth_cache.update_time
+            bids = pd.DataFrame(depth_cache.get_bids()[:5], columns=["Price", "Quantity"])
+            asks = pd.DataFrame(depth_cache.get_asks()[:5], columns=["Price", "Quantity"])
+
+            print(bids)
+
+        depth_watcher = dcm.start_depth_cache(on_input_message, symbol=coin_symbol)
+
+        dcm.join()
+
+    except Exception as err:
+        print("Ошибка подключения к базе данных:")
+        print(err)
+
+
+if __name__ == "__main__":
+    main()
